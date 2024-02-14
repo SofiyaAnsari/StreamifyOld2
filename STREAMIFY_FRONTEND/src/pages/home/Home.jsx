ac154ff908ac02e7a728aab1e9acfc2813662a60
hiralbarot6797 <hiralbarot6797@gmail.com>
2024-02-13 19:10:39 -0500

    Fetch Data From API

--- STREAMIFY_FRONTEND/src/pages/home/Home.jsx
+++ STREAMIFY_FRONTEND/src/pages/home/Home.jsx
@@ -2,18 +2,8 @@ import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./home.scss";
 import List from "../../components/list/List";
+
 const Home = () => {
-	const [lists, setLists] = useState([]);
-
-	useEffect(()=>{
-		const getRandomLists = async () =>{
-			try{
-
-			}catch(err){
-				console.log(err)
-			}
-		}
-	})
 	return (
 		<div className="home">
 			<Navbar />
