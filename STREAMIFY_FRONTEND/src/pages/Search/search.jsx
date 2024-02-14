88495ac23037dbdc81ccbb033d30bccea0f00d6e
alvinparmar <alvinparmar100@gmail.com>
2024-02-13 20:23:52 -0500

    new API

---
+++ STREAMIFY_FRONTEND/src/pages/Search/search.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class search extends Component {
+  render() {
+	return (
+	  <div>
+
+        <h1> This is search page </h1>
+		
+	  </div>
+	)
+  }
+}
